dfa8845de1e7050c00fd02f68e61503653044e64
Jaychaware <44925830+Jaychaware@users.noreply.github.com>
2025-03-18 16:20:43 +0530

    Add files via upload

---
+++ artifacts/conv.py
@@ -0,0 +1,11 @@
+import json
+
+from cmflib import cmfquery
+
+query = cmfquery.CmfQuery('/home/jaychaware/cmf-server/data/mlmd')
+json_payload = query.dumptojson(
+                'Test-env',None
+)
+print(json.dumps(json.loads(json_payload),indent=4))
+with open('test_mlmd_100_cmf.txt','w') as f:
+    f.write(json.dumps(json.loads(json_payload),indent=4))
